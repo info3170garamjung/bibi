1968a6da251aa58eea9d6144d8abc150463e776b
info3170garamjung <g_jung155465@fanshaweonline.ca>
2023-06-26 18:53:31 -0400

    antd -> li ul

--- front/components/Category.js
+++ front/components/Category.js
@@ -1,4 +1,4 @@
-
+/*
 import React, {useState} from "react";
 import { 
   RightOutlined,
@@ -87,3 +87,96 @@ const handleCategoryClick = (category) => {
 
 export default Category;
 
+*/
+
+import React, { useState } from 'react';
+import { useRouter } from 'next/router';
+import styled from 'styled-components';
+import Link from 'next/link';
+import { useDispatch, useSelector } from 'react-redux';
+import { showPostFormAction } from '../reducers/post';
+import { categories } from './categories';
+import GlobalStyles from './GlobalStyles';
+import { Divider } from 'antd';
+
+const MenuContainer = styled.div`
+  // menu-container 스타일
+`;
+
+const MenuItem = styled.li`
+  // menu-item 스타일
+  list-style-type: none;
+  
+  font-weight: ${({ isSelected }) => (isSelected ? 'bold' : 'normal')};
+  cursor: pointer;
+
+  &:hover {
+    font-weight: bold;
+  }
+`;
+
+
+const SubmenuTitle = styled.span`
+  // submenu-title 스타일
+`;
+
+const SubmenuItems = styled.ul`
+  // submenu-items 스타일
+  list-style-type: none;
+  padding-left: 0; 
+`;
+const MenuList = styled.ul`
+  // menu 스타일
+  list-style-type: none; // 목록 동그라미 제거
+`;
+
+const Category = () => {
+  const { showPostForm } = useSelector((state) => state.post);
+  const router = useRouter();
+  const dispatch = useDispatch();
+
+  const handleCategoryClick = (category) => {
+    if (category.title === 'All') {
+      router.push('/category/all_posts'); // replace with actual path
+    } else {
+      router.push(`/category/${category.title}`);
+    }
+    if (showPostForm) {
+      dispatch(showPostFormAction(false));
+    }
+  };
+
+
+  return (
+    <>
+      <GlobalStyles />
+      <MenuContainer className="menu-container">
+      <MenuList className="menu">
+          <li className="submenu" style={{marginTop: '2.5rem'}}>
+            <SubmenuTitle className="submenu-title" style={{ fontFamily: 'Bitter', fontSize: '1.1rem' }}>Tag list</SubmenuTitle>
+            <Divider />
+            <SubmenuItems className="submenu-items" >
+              {categories.map((category) => (
+                <MenuItem
+                  key={category.key}
+                  onClick={() => handleCategoryClick(category)}
+                  className="menu-item"
+                  isSelected={router.asPath === `/category/${category.title}`}
+                  style={{marginBottom: '1rem', fontFamily: 'Bitter'}}
+                >
+                  {category.href ? (
+                    <Link href={category.href}>{category.title.toLowerCase()}</Link>
+                  ) : (
+                    category.title
+                  )}
+                </MenuItem>
+              ))}
+            </SubmenuItems>
+          </li>
+        </MenuList>
+      </MenuContainer>
+    </>
+  );
+};
+
+export default Category;
